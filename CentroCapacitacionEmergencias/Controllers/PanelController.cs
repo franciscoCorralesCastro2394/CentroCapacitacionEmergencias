2335232ee07fee11f1910208d69e06b093093a59
Francisco <chikocorrales@hotmail.com>
2026-03-23 01:18:17 -0600

    cambios en panle de visualizacion

---
+++ CentroCapacitacionEmergencias/Controllers/PanelController.cs
@@ -0,0 +1,118 @@
+﻿using Antlr.Runtime.Tree;
+using CentroCapacitacionEmergencias.Data;
+using CentroCapacitacionEmergencias.Models;
+using CentroCapacitacionEmergencias.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CentroCapacitacionEmergencias.Controllers
+{
+    public class PanelController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Panel
+        public ActionResult Index(int? idCurso, int? idCohorte)
+        {
+            int intructorId = (int)Session["UserId"];
+
+            //Cohortes
+            List<EvaluacionDestreza> evaluacionCohortes = db.EvaluacionDestreza.Where(e => e.CursoId == 
+                                                                 idCurso && e.InstructorId == intructorId
+                                                                 ).ToList();
+            ViewBag.EvaluacionCohortes = evaluacionCohortes;
+
+
+            var cursos = db.Cursos.Where(c => c.Id == idCurso).ToList();
+
+            ViewBag.Cursos = cursos;
+
+            var cohorte = db.Cohortes.Where(c => c.Id == idCohorte).ToList();
+            ViewBag.Cohorte = cohorte;
+
+
+            List<Participante> participantesporCurso = db.ParticipanteCursos.Where(pc => pc.CursoId == idCurso)
+                                        .Select(pc => pc.Participante)
+                                        .ToList();
+
+            ViewBag.ParticipantesCurso = participantesporCurso;
+
+            List<Participante> participantesCohorte = db.ParticipanteCohortes.Where(pc => pc.CohorteID == idCohorte)
+                                        .Select(pc => pc.Participante)
+                                        .ToList();
+            ViewBag.ParticipantesCohorte = participantesCohorte;
+
+
+            int evaluacionesTotales = evaluacionCohortes.Count();
+            ViewBag.EvaluacionesTotales = evaluacionesTotales;
+
+            int putajeMinimo = 70; // Ejemplo de puntaje mínimo para aprobar
+            ViewBag.PuntajeMinimo = putajeMinimo;
+
+            var aprobadas = evaluacionCohortes.Where(e => e.PuntajeFinal >= putajeMinimo).ToList();
+            ViewBag.Aprobadas = aprobadas;
+
+
+            double tasa = evaluacionesTotales > 0 ? (double)aprobadas.Count() / evaluacionesTotales * 100 : 0;
+            ViewBag.TasaAprobacion = tasa;
+            ///
+
+            int certificados = aprobadas.Count();
+            ViewBag.Certificados = certificados;
+
+            int horas = 0;
+            ViewBag.Horas = horas;
+
+            foreach (var item in aprobadas)
+            {
+                var cursoAprobado = db.Cursos.FirstOrDefault(c => c.Id == item.CursoId);
+                if (cursoAprobado != null)
+                {
+                    horas += cursoAprobado.HorasPracticas;
+                }
+            }
+
+
+
+            return View();
+        }
+
+        public ActionResult Seleccion() 
+        {
+
+            int intructorId = (int)Session["UserId"];
+
+            List<Curso> cursosinstructor = db.Cursos.Where(c => c.Instructores.Any(i => i.Id == intructorId)).ToList();
+
+
+            // Obtener los cursos asociados al instructor
+            var cursos = cursosinstructor.Select(c => new SelectListItem { 
+                                       Value = c.Id.ToString(),
+                                       Text = c.Titulo
+                                       }).ToList();
+
+            // Obtener las cohortes asociadas a los cursos del instructor
+            var cohortes = cursosinstructor.SelectMany(c => c.Cohortes).
+                                            Select(co => new SelectListItem
+                                            {
+                                                Value = co.Id.ToString(),
+                                                Text = co.Nombre
+
+                                            }).ToList();
+
+
+
+
+            ViewBag.Cursos = cursos;
+
+
+            ViewBag.Cohortes = cohortes;
+
+            return View();
+
+        }
+    }
+}
